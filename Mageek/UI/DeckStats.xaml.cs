dc0af4703d338b747721da4ef8654db5d7e4a1f5
Nihilhis <pierre.plaziat@live.fr>
2022-07-23 00:03:39 +0200

    prepared deck stats control

---
+++ Mageek/UI/DeckStats.xaml.cs
@@ -0,0 +1,79 @@
+﻿using MaGeek.Data.Entities;
+using MaGeek.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaGeek.UI
+{
+
+    public partial class DeckStats : UserControl, INotifyPropertyChanged
+    {
+
+        #region PropertyChange
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+
+        private MagicDeck currentDeck;
+        public MagicDeck CurrentDeck
+        {
+            get { return currentDeck; }
+            set
+            {
+                currentDeck = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Visible");
+            }
+        }
+
+        public Visibility Visible
+        {
+            get { return currentDeck == null ? Visibility.Visible : Visibility.Collapsed; }
+        }
+
+        public DeckStats()
+        {
+            InitializeComponent();
+            DataContext = this;
+            App.state.RaiseSelectDeck += HandleDeckSelected;
+            App.state.RaiseDeckModif += HandleDeckModified;
+        }
+
+        void HandleDeckModified(object sender, DeckModifEventArgs e)
+        {
+            FullRefresh();
+        }
+
+        void HandleDeckSelected(object sender, SelectDeckEventArgs e)
+        {
+            CurrentDeck = e.Deck;
+            FullRefresh();
+        }
+
+        private void FullRefresh()
+        {
+            CurrentDeck = App.state.SelectedDeck;
+        }
+
+    }
+
+}
