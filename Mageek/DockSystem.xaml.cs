26185de128867695125730fd31f1876f8931876e
Nihilhis <pierre.plaziat@live.fr>
2022-08-18 19:44:48 +0200

    Reintroducing avalon

---
+++ Mageek/DockSystem.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MaGeek
+{
+    /// <summary>
+    /// Logique d'interaction pour DockSystem.xaml
+    /// </summary>
+    public partial class DockSystem : Window
+    {
+        public DockSystem()
+        {
+            InitializeComponent();
+        }
+    }
+}
