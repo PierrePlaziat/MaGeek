f1b59c7f3ba0f01774ff25fc848c40df52c9df23
Nihilhis <pierre.plaziat@live.fr>
2022-08-21 21:01:35 +0200

    New Layout

--- Mageek/DockSystem.xaml.cs
+++ Mageek/DockSystem.xaml.cs
@@ -21,7 +21,17 @@ namespace MaGeek
     {
         public DockSystem()
         {
+            DataContext = this;
             InitializeComponent();
         }
+
+        private bool cardInspectorOpened = true;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cardInspectorOpened) BaseGrid.ColumnDefinitions[BaseGrid.ColumnDefinitions.Count - 1].Width = new GridLength(0);
+            else BaseGrid.ColumnDefinitions[BaseGrid.ColumnDefinitions.Count - 1].Width = new GridLength(255);
+            cardInspectorOpened = !cardInspectorOpened;
+        }
+
     }
 }
